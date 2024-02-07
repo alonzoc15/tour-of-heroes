43f94e9c567232ea4d53a54948ba03095f12656f
Alonzo Camille <alonzocamille15@gmail.com>
2024-02-06 20:50:44 -0500

    binding sidekick and hero route

--- src/app/sidekick-detail/sidekick-detail.component.ts
+++ src/app/sidekick-detail/sidekick-detail.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { SidekickService } from '../sidekick.service';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-sidekick-detail',
@@ -12,21 +14,30 @@ import { SidekickService } from '../sidekick.service';
 })
 export class SidekickDetailComponent {
   sidekick: Sidekick | undefined;
+  hero: Hero | undefined;
 
   constructor(
     private route: ActivatedRoute,
     private sidekickService: SidekickService,
+    private heroService: HeroService,
     private location: Location
   ) {}
 
   ngOnInit(): void {
     this.getSidekick();
+    this.getHero();
   }
   
   getSidekick(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.sidekickService.getSidekick(id)
       .subscribe(sidekick => this.sidekick = sidekick);
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
